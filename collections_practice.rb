6e3af0ae9c4ab5e27530ce11094d7ef5cf1d08ff
Chantel Lowe <chantellowe17@gmail.com>
2019-08-04 18:32:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length  
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |letter|
+    letter[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
